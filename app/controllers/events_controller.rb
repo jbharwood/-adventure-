3b9006b93517888f9f02d7355f892e2524416e52
Your Name <github email address>
2019-02-12 17:23:16 -0500

    search functionality by category on events index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,15 +2,35 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+    if params["type"] == "1"
+      @events = Event.where(event_type: "Happy Hour")
+    elsif params["type"] == "2"
+      @events = Event.where(event_type: "Escape Room")
+    else
+      @events = Event.all
+    end
   end
 
   def show
     @event = Event.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @event = Event.create(event_params)
+    redirect_to @event
+  end
+
   def update
     byebug
   end
 
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :location_id, :event_type)
+  end
 
 end
